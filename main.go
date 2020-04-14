d0abb44ebd156260fb91717051659765dbe76c88
jhamPac <cloud@kevia.me>
2020-04-13 19:52:23 -0700

    creating a custom slice type

--- main.go
+++ main.go
@@ -12,14 +12,44 @@ type ActionHistory struct {
 	size int
 }
 
-// Apply add to the stack
-func (ah *ActionHistory) Apply(newAction *Action) {
+type Luggage struct {
+	weight    int
+	passenger string
+}
+
+func NewLuggage(weight int, passenger string) *Luggage {
+	l := Luggage{weight, passenger}
+	return &l
+}
+
+type Belt []*Luggage
+
+func (b *Belt) Take() *Luggage {
+	first, rest := (*b)[0], (*b)[1:]
+	*b = rest
+	return first
+}
+
+// Add to the stack
+func (ah *ActionHistory) Add(newAction *Action) {
 	if ah.top != nil {
 		oldTop := ah.top
 		newAction.next = oldTop
 	}
 	ah.top = newAction
 	ah.size++
+}
+
+// Undo the last action
+func (ah *ActionHistory) Undo() *Action {
+	topAction := ah.top
+	if topAction != nil {
+		ah.top = topAction.next
+	} else if topAction.next == nil {
+		ah.top = nil
+	}
+	ah.size--
+	return topAction
 }
 
 func main() {
